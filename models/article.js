295ff72f2b6372b9423e5d17366169b8e94685ab
Daniel JS <daniel@mydeveloperjourney.com>
2022-05-27 14:38:58 -0500

    feat: adds article model

---
+++ models/article.js
@@ -0,0 +1,26 @@
+// require dependencies
+const mongoose = require('mongoose');
+// initialize shortcut variable
+const Schema = mongoose.Schema;
+
+// initialize schema
+const articleSchema = new Schema({
+    title: String,
+    body: String
+});
+
+// export our model
+module.exports = mongoose.model('Article', articleSchema);
+
+
+// ^-- this code takes a schema and turns it into a model
+// this will provide us with a bunch of special methods
+/*
+    These model methods allow us to perform crud on a specific collection in a database
+    
+    Model.find()
+    Model.create()
+    Model.findById()
+    Model.findByIdUpdate()
+    Model.findByIdDelete()
+*/
