b884eb7ed4b30ba6a116eefd1e27f1f8731adce3
Daniel JS <daniel@mydeveloperjourney.com>
2022-05-27 14:13:17 -0500

    wip: adds new route

--- controllers/articles.js
+++ controllers/articles.js
@@ -3,8 +3,15 @@
 const router = require('express').Router();
 
 // define routes
+
+// Index
 router.get('/', (req, res) => {
     res.render('articles/index.ejs');
+});
+
+// New
+router.get('/new', (req, res) => {
+    res.render('articles/new.ejs');
 });
 
 // export our router object
